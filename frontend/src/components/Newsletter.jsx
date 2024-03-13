b6e03b36dabcab54bbd519f047432e9c72aef480
Vivek Warkade <vivekwarkade000@gmail.com>
2024-03-13 23:10:13 +0530

    newsletter changes

--- frontend/src/components/Newsletter.jsx
+++ frontend/src/components/Newsletter.jsx
@@ -5,14 +5,12 @@ const Newsletter = () => {
       <div name='newsletter' className='w-full md:h-screen text-gray-300 bg-[#14101b]'>
         <div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full'>
             <div className='pb-8'>
-                <p className='text-4xl font-bold inline border-b-4 text-gray-300 border-teal-600'>Work</p>
-                <p className='py-6'>Newsletter</p>
+                <p className='text-4xl font-bold inline border-b-4 text-gray-300 border-teal-600'>Newsletter</p>
             </div>
-            <div>
-              <form className="newsletter-form bg-[#14101b]">
-                <label htmlFor="email">Email Address:</label>
-                <input type="email" id="email" required />
-                <button type="submit">Subscribe</button>
+            <div name="newsletter">
+              <form className=" bg-[#14101b] items-center">
+                <input type="email" id="email" placeholder='Enter Your Email' required />
+                <button type="submit" className='text-white border-2 hover:bg-teal-600 hover:border-teal-600 px-3 py-2 mx-auto flex items-center'>Subscribe</button>
               </form>
             </div>
         </div>
