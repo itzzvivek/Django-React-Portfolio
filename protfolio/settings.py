e0825a77b91a02721e87c3268f415d2de265a91c
Vivek Warkade <vivekwarkade000@gmail.com>
2024-03-25 16:13:00 +0530

    changes

--- protfolio/settings.py
+++ protfolio/settings.py
@@ -56,6 +56,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'protfolio.urls'
@@ -76,7 +77,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'protfolio.wsgi.application'
+WSGI_APPLICATION = 'protfolio.wsgi.app'
 
 
 # Database
